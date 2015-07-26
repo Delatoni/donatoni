739865bb87262ea192ec3b9584298309732a625b
Dell Donatoni <ddonatoni@elevatestudios.com>
2015-07-26 15:36:34 -0500

    Added: Base JS for sidebars.

--- js/main.js
+++ js/main.js
@@ -54,4 +54,40 @@ $(document).ready(function() {
         userFeed.next();
     });
 
+    /* jQuery Sidebar Code */
+
+    // All sides
+    var sides = ["left", "top", "right", "bottom"];
+
+    // Initialize sidebars
+    for (var i = 0; i < sides.length; ++i) {
+        var cSide = sides[i];
+        $(".sidebar." + cSide).sidebar({side: cSide});
+    }
+
+    // Click handlers
+    $(".trigger[data-action]").on("click", function () {
+        var $this = $(this);
+        var action = $this.attr("data-action");
+        var side = $this.attr("data-side");
+        $(".sidebar." + side).trigger("sidebar:" + action);
+        return false;
+    });
+
+    /* Wow.js Code */
+    var wow = new WOW(
+        {
+        boxClass:     'wow',      // animated element css class (default is wow)
+        animateClass: 'animated', // animation css class (default is animated)
+        offset:       50,          // distance to the element when triggering the animation (default is 0)
+        mobile:       true,       // trigger animations on mobile devices (default is true)
+        live:         true,       // act on asynchronously loaded content (default is true)
+        callback:     function(box) {
+            // the callback is fired every time an animation is started
+            // the argument that is passed in is the DOM node being animated
+        }
+    }
+    );
+    wow.init();
+
 });
